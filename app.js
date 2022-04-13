3db17a60b28173a79308748d1c1fa58cfa0b1b28
fedetalla <fedetalla@hotmail.com>
2022-04-13 19:57:51 -0300

    agregue procfile

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.listen(3100, () => {
+app.listen(process.env.PORT || 3100, () => {
     console.log("Servidor corriendo en http://localhost:3100")
 })
 
@@ -19,3 +19,4 @@ app.post("/register", (req, res) => {
 app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
+
