457575566474946eed62d14b46fa7078a768e3d0
fedetalla <fedetalla@hotmail.com>
2022-04-13 18:51:21 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+
+app.listen(3100, () => {
+    console.log("Servidor corriendo en http://localhost:3100")
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/index.html"))
+})
+
+app.post("/register", (req, res) => {
+    res.sendFile (path.resolve(__dirname, "./views/index.html"));
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
